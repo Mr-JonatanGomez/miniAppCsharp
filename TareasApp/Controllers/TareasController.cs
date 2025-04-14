6881e7542aa01b9605109454f7a0efc1583797ec
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2025-04-15 01:25:45 +0200

    actualizando acabado con botones de editar y eliminar

--- TareasApp/Controllers/TareasController.cs
+++ TareasApp/Controllers/TareasController.cs
@@ -41,6 +41,93 @@ namespace TareasApp.Controllers
             }
             return View(tarea);
         }
+        //GET para EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null || _context.Tareas == null)
+            {
+                return NotFound();
+            }
+            var tarea = await _context.Tareas.FindAsync(id);
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+            return View(tarea);
+        }
 
+
+        //POST: para editar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdTarea, Titulo, Descripcion")]Tarea tarea)
+        {
+
+            if( id != tarea.IdTarea) 
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid) 
+            {
+                try 
+                {
+                    _context.Update(tarea);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index)); 
+                } 
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TareaExists(tarea.IdTarea))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            return View(tarea);
+        }
+
+        private bool TareaExists(int id)
+        {
+            return _context.Tareas.Any(e => e.IdTarea == id);
+        }
+
+
+        //GET: TAREAS Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id==null || _context.Tareas == null)
+            {
+                return NotFound();
+            }
+            var tarea = await _context.Tareas.FindAsync(id);
+            if(tarea == null)
+            {
+                return NotFound();  
+            }
+            return View(tarea);
+        }
+
+        //POST: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if(_context.Tareas == null)
+            {
+                return Problem("Entidad a borrar es null");
+            }
+            var tarea = await _context.Tareas.FindAsync(id);
+            if(tarea != null)
+            {
+                _context.Tareas.Remove(tarea); 
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
+
 }
